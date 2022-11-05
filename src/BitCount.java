01eef09e631308233e90b76c504233cefb49a883
gdtknight <gdtknight@naver.com>
2022-11-05 12:20:29 +0900

    BitCount.java

---
+++ src/BitCount.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class BitCount {
+  public static void main(String[] args) {
+    int n = 1_000_000;
+    Integer.bitCount(n);
+
+    int[] arr = new int[] { 1, 2, 3 };
+    Arrays.sort(arr);
+  }
+}
